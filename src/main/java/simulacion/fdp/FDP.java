4753ca51074ab6c943ecae9d0896a882f8b07fe1
yankkok <igfernandezsoto@gmail.com>
2018-05-06 23:15:51 -0300

    GITIGNORE DE MIERDA

--- src/main/java/simulacion/fdp/FDP.java
+++ src/main/java/simulacion/fdp/FDP.java
@@ -5,10 +5,12 @@ import java.math.BigDecimal;
 public abstract class FDP {
 	
 	public BigDecimal obtenerValor(double probabilidad) {
+				
+		double exponenteDeE = -Math.pow((Math.log(probabilidad) - mu().doubleValue()), 2) / (2 * Math.pow(sigma().doubleValue(), 2));
+		BigDecimal numerador = new BigDecimal(Math.pow(Math.E, exponenteDeE));
+		BigDecimal denominador = sigma().multiply(new BigDecimal(probabilidad)).multiply(new BigDecimal(Math.sqrt(2 * Math.PI)));
 		
-		
-		
-		return null;
+		return numerador.divide(denominador);
 		
 	}
 	
